29305824f48c129337c18af25dfb881c2dca1f6e
Javier Cruz <javi.cb@gmail.com>
2021-08-12 13:56:57 +0200

    diseño de componentes

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2>Pokemon picture</h2>
+  <div class="pokemon-container">
+    <img id="pokemon"
+         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="fade-in"
+         alt="img-pokemon">
+    <img id="pokemon-oculto"
+         src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+         class="hidden-pokemon"
+         alt="img-pokemon">
+  </div>
 </template>
 
 <script>
@@ -8,6 +17,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
+.pokemon-container {
+    height: 200px;
+    display: flex;
+    justify-content: center;
+}
+
+img {
+    width: 50%;
+    user-select: none;
+    position: absolute;
+    margin-right: auto;
+    margin-left: auto;
+    height: 21%;
+    display: block;
+    -webkit-user-select: none;
+    -webkit-user-drag: none;
+    -ms-user-select: none;
+    -moz-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
