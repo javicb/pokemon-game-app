0d8c7052f4eab02b4fcfed712abb2264627fec97
Javier Cruz <javi.cb@gmail.com>
2021-08-12 15:21:58 +0200

    obtener nombre de los 4 pokemon

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,3 +1,5 @@
+import pokemonAPI from "../api/pokemonAPI"
+
 // Creamos el array
 const getPokemons = () => {
   const pokemons = Array.from(Array(650))
@@ -6,14 +8,32 @@ const getPokemons = () => {
 }
 
 // Mezclamos el array de pokemon
-const getPokemonOptions = () => {
+const getPokemonOptions = async () => {
   const mixedPokemon = getPokemons().sort(() => Math.random() - 0.5)
-  getPokemonName(mixedPokemon.splice(0, 4))
+  const pokemons = await getPokemonName(mixedPokemon.splice(0, 4))
+  return pokemons
 }
 
 // Obtenemos el nombre del pokemon
-const getPokemonName = ([a, b, c, d] = []) => {
-  console.log(a, b, c, d)
+const getPokemonName = async ([a, b, c, d] = []) => {
+
+  const promise = [
+    pokemonAPI.get(`/${a}`),
+    pokemonAPI.get(`/${b}`),
+    pokemonAPI.get(`/${c}`),
+    pokemonAPI.get(`/${d}`),
+  ]
+
+  const [poke1, poke2, poke3, poke4] = await Promise.all(promise)
+
+  return [
+    { name: poke1.data.name, id: poke1.data.id },
+    { name: poke2.data.name, id: poke2.data.id },
+    { name: poke3.data.name, id: poke3.data.id },
+    { name: poke4.data.name, id: poke4.data.id }
+  ]
+
+
 }
 
 
